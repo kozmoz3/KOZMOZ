e03963f98014e5e22bdb33f7ad4ba1bfec3eead3
ENRIQUESOSA <kiqueyo847@gmail.com>
2016-10-01 00:47:08 -0500

    prueba2

--- KosmozBus/Datos/Administrador/MenuAdmin.Designer.cs
+++ KosmozBus/Datos/Administrador/MenuAdmin.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // MenuAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(802, 323);
+            this.Controls.Add(this.label1);
             this.Name = "MenuAdmin";
             this.Text = "Menu Administrador";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
