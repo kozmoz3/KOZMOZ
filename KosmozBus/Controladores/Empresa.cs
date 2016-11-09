be21c3ecd6399f92e8db6de2936de5e8176f123d
ENRIQUESOSA <kiqueyo847@gmail.com>
2016-11-09 10:20:43 -0600

    desde visual

---
+++ KosmozBus/Controladores/Empresa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using DataModel;
+using Vistas;
+using System.Windows.Forms;
+
+
+namespace Controladores
+{
+    public class Empresa
+    {
+        public void  insert(empresaoperadoraDto dta)
+        {
+            try { 
+                using(kosmozbusEntities db = new kosmozbusEntities())
+                {
+                    
+                    
+                   /// MessageBox.Show(""+fechaHoy);
+                    int a;
+
+                   // DateTime fechaConversion = DateTime.Parse(this.lbFecha.Text);
+                }
+            
+            }catch(Exception e){
+                MessageBox.Show("Error al insertar empresa operadora ->"+e);
+            }
+
+        }
+    }
+}
